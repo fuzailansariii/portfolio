bea9a1c1481bae52bf6ecd07226da968df0b3a9e
fuzailansariii <fuzailansarisecret@gmail.com>
2025-03-01 21:50:42 +0530

    Resume page designed

--- src/app/(pages)/resume/page.tsx
+++ src/app/(pages)/resume/page.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import ResumeTemplate from "@/app/components/ResumeTemplate";
+import ResumeItems from "@/app/components/ResumeItems";
 import React from "react";
 
 export default function ResumePage() {
   return (
     <div className="md:max-w-screen-lg flex flex-col mx-auto p-5">
       <h1 className="text-4xl my-10 font-bold">Resume</h1>
-      <ResumeTemplate
-        heading="Education"
-        year="2021"
-        title="B.Tech"
-        description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sapiente
-      expedita pariatur laborum! Expedita sunt quae quia similique, iusto
-      reiciendis enim, voluptatem perferendis accusantium quo, possimus corrupti
-      impedit quos sed?"
-      />
+      <ResumeItems heading="Education" />
+      <div className="w-full bg-white border-b" />
+      <ResumeItems heading="Skills & Experties" />
     </div>
   );
 }
